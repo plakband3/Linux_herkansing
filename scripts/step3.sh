6b66ec92dc3da08ae1effec9d7ea3f67ebeb95f2
Joost <Joostholwerda@hotmail.com>
2019-07-07 18:31:15 +0200

    FINAL PUSH :D

--- scripts/step3.sh
+++ scripts/step3.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Install and configure Git
-
-# Login minion and use pull_git.sh to pull git on ubuntu master
-sudo nano /home/joostholwerda/pull_git.sh
-
-# manually copy script and paste in master pull_git.sh
-# save, confirm
-sudo chmod +x /home/joostholwerda/pull_git.sh
-sudo /home/joostholwerda/pull_git.sh
-
-
 # Install and configure Salt
 sudo chmod +x /home/joostholwerda/git/linux-herkansing/scripts/salt_minion_installation.sh
 sudo /home/joostholwerda/git/linux-herkansing/scripts/salt_minion_installation.sh
